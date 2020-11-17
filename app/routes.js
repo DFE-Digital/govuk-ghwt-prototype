b309fa761d75ba40b1d83052f99d02287b52ba53
Mel Ashby <melanie.ashby@sigma.se>
2020-11-17 15:43:54 +0000

    Restructure to begin implementing full website prototype

--- app/routes.js
+++ app/routes.js
@@ -7,39 +7,9 @@ router.use(radioButtonRedirect)
 
 // Add your routes here - above the module.exports line
 
-router.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
 
-router.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
 
-  // get the url pathname
-  var pathname = parseurl(req).pathname
 
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-  next()
-})
-
-// output url for session data
-
-// router.get('/support-details', function(req, res) {
-//   res.render('/support-details', { 'support-user-entry' : 'Foo' });
-// });
-
-router.get('/layout', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/confirm'] + ' times')
-})
-
-router.get('/layout', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/describe'] + ' times')
-})
 
 
 module.exports = router
